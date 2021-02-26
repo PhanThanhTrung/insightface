fb73afcf69de7e8d7987035e250e4bea48a0d925
Miles <trung99nb@gmail.com>
2021-02-26 15:59:13 +0700

    ddkm

--- recognition/ArcFace/verification.py
+++ recognition/ArcFace/verification.py
@@ -805,4 +805,6 @@ if __name__ == '__main__':
                     (ver_name_list[i], acc2, std2))
             print(f'[{ver_name_list[i]}]Mean threshold: {best_thr_acc}')
             results.append(acc2)
-        print('Max of [%s] is %1.5f' % (ver_name_list[i], np.max(results[i])))
+            print(len(results))
+            results = np.array(results)
+        print('Max of [%s] is %1.5f' % (ver_name_list[i], np.max(results)))
